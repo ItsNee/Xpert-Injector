792495cd4c675790ac681280c2c73099245f2d53
XpertNee <neeran92@gmail.com>
2018-04-02 14:54:50 +0800

    Included all files
    ~nil~

---
+++ Form1.Designer.vb
@@ -0,0 +1,262 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.MonoFlat_ThemeContainer1 = New Xpert_Injector.MonoFlat.MonoFlat_ThemeContainer()
+        Me.Checkbox1 = New Xpert_Injector.MonoFlat.MonoFlat_CheckBox()
+        Me.Label1 = New Xpert_Injector.MonoFlat.MonoFlat_Label()
+        Me.RadioButton2 = New Xpert_Injector.MonoFlat.MonoFlat_RadioButton()
+        Me.Button2 = New Xpert_Injector.MonoFlat.MonoFlat_Button()
+        Me.Button5 = New Xpert_Injector.MonoFlat.MonoFlat_Button()
+        Me.Button4 = New Xpert_Injector.MonoFlat.MonoFlat_Button()
+        Me.Button3 = New Xpert_Injector.MonoFlat.MonoFlat_Button()
+        Me.RadioButton1 = New Xpert_Injector.MonoFlat.MonoFlat_RadioButton()
+        Me.Button1 = New Xpert_Injector.MonoFlat.MonoFlat_Button()
+        Me.DLLs = New System.Windows.Forms.ListBox()
+        Me.MonoFlat_ControlBox1 = New Xpert_Injector.MonoFlat.MonoFlat_ControlBox()
+        Me.Textbox1 = New Xpert_Injector.MonoFlat.MonoFlat_TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.MonoFlat_ThemeContainer1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MonoFlat_ThemeContainer1
+        '
+        Me.MonoFlat_ThemeContainer1.BackColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.MonoFlat_ThemeContainer1.Controls.Add(Me.Checkbox1)
+        Me.MonoFlat_ThemeContainer1.Controls.Add(Me.Label1)
+        Me.MonoFlat_ThemeContainer1.Controls.Add(Me.RadioButton2)
+        Me.MonoFlat_ThemeContainer1.Controls.Add(Me.Button2)
+        Me.MonoFlat_ThemeContainer1.Controls.Add(Me.Button5)
+        Me.MonoFlat_ThemeContainer1.Controls.Add(Me.Button4)
+        Me.MonoFlat_ThemeContainer1.Controls.Add(Me.Button3)
+        Me.MonoFlat_ThemeContainer1.Controls.Add(Me.RadioButton1)
+        Me.MonoFlat_ThemeContainer1.Controls.Add(Me.Button1)
+        Me.MonoFlat_ThemeContainer1.Controls.Add(Me.DLLs)
+        Me.MonoFlat_ThemeContainer1.Controls.Add(Me.MonoFlat_ControlBox1)
+        Me.MonoFlat_ThemeContainer1.Controls.Add(Me.Textbox1)
+        Me.MonoFlat_ThemeContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.MonoFlat_ThemeContainer1.Font = New System.Drawing.Font("Stencil", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MonoFlat_ThemeContainer1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(95, Byte), Integer), CType(CType(105, Byte), Integer), CType(CType(104, Byte), Integer))
+        Me.MonoFlat_ThemeContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.MonoFlat_ThemeContainer1.Name = "MonoFlat_ThemeContainer1"
+        Me.MonoFlat_ThemeContainer1.Padding = New System.Windows.Forms.Padding(10, 70, 10, 9)
+        Me.MonoFlat_ThemeContainer1.RoundCorners = True
+        Me.MonoFlat_ThemeContainer1.Sizable = True
+        Me.MonoFlat_ThemeContainer1.Size = New System.Drawing.Size(496, 456)
+        Me.MonoFlat_ThemeContainer1.SmartBounds = True
+        Me.MonoFlat_ThemeContainer1.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultLocation
+        Me.MonoFlat_ThemeContainer1.TabIndex = 0
+        Me.MonoFlat_ThemeContainer1.Text = "Xpert Injector V2.1"
+        '
+        'Checkbox1
+        '
+        Me.Checkbox1.Checked = False
+        Me.Checkbox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!)
+        Me.Checkbox1.Location = New System.Drawing.Point(13, 347)
+        Me.Checkbox1.Name = "Checkbox1"
+        Me.Checkbox1.Size = New System.Drawing.Size(148, 16)
+        Me.Checkbox1.TabIndex = 29
+        Me.Checkbox1.Text = "Close on INJECT"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(116, Byte), Integer), CType(CType(125, Byte), Integer), CType(CType(132, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(10, 70)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 15)
+        Me.Label1.TabIndex = 28
+        Me.Label1.Text = "Status:"
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.Checked = False
+        Me.RadioButton2.Font = New System.Drawing.Font("Showcard Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton2.Location = New System.Drawing.Point(104, 314)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(68, 17)
+        Me.RadioButton2.TabIndex = 27
+        Me.RadioButton2.Text = "Auto"
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Transparent
+        Me.Button2.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.Button2.Image = Nothing
+        Me.Button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button2.Location = New System.Drawing.Point(333, 207)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(146, 41)
+        Me.Button2.TabIndex = 26
+        Me.Button2.Text = "Remove"
+        Me.Button2.TextAlignment = System.Drawing.StringAlignment.Center
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.Transparent
+        Me.Button5.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.Button5.Image = Nothing
+        Me.Button5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button5.Location = New System.Drawing.Point(333, 104)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(146, 41)
+        Me.Button5.TabIndex = 25
+        Me.Button5.Text = "Quit"
+        Me.Button5.TextAlignment = System.Drawing.StringAlignment.Center
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.Transparent
+        Me.Button4.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.Button4.Image = Nothing
+        Me.Button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button4.Location = New System.Drawing.Point(313, 373)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(166, 57)
+        Me.Button4.TabIndex = 24
+        Me.Button4.Text = "Inject"
+        Me.Button4.TextAlignment = System.Drawing.StringAlignment.Center
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.Transparent
+        Me.Button3.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.Button3.Image = Nothing
+        Me.Button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button3.Location = New System.Drawing.Point(333, 254)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(146, 41)
+        Me.Button3.TabIndex = 23
+        Me.Button3.Text = "Clear List"
+        Me.Button3.TextAlignment = System.Drawing.StringAlignment.Center
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.Checked = False
+        Me.RadioButton1.Font = New System.Drawing.Font("Showcard Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton1.Location = New System.Drawing.Point(13, 314)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(85, 17)
+        Me.RadioButton1.TabIndex = 21
+        Me.RadioButton1.Text = "Manual"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Transparent
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.Button1.Image = Nothing
+        Me.Button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button1.Location = New System.Drawing.Point(333, 160)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(146, 41)
+        Me.Button1.TabIndex = 16
+        Me.Button1.Text = "Browse"
+        Me.Button1.TextAlignment = System.Drawing.StringAlignment.Center
+        '
+        'DLLs
+        '
+        Me.DLLs.BackColor = System.Drawing.Color.FromArgb(CType(CType(65, Byte), Integer), CType(CType(75, Byte), Integer), CType(CType(84, Byte), Integer))
+        Me.DLLs.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.DLLs.Font = New System.Drawing.Font("Stencil", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DLLs.ForeColor = System.Drawing.Color.FromArgb(CType(CType(176, Byte), Integer), CType(CType(183, Byte), Integer), CType(CType(191, Byte), Integer))
+        Me.DLLs.FormattingEnabled = True
+        Me.DLLs.ItemHeight = 22
+        Me.DLLs.Location = New System.Drawing.Point(13, 160)
+        Me.DLLs.Name = "DLLs"
+        Me.DLLs.Size = New System.Drawing.Size(314, 132)
+        Me.DLLs.TabIndex = 15
+        '
+        'MonoFlat_ControlBox1
+        '
+        Me.MonoFlat_ControlBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.MonoFlat_ControlBox1.EnableHoverHighlight = False
+        Me.MonoFlat_ControlBox1.EnableMaximizeButton = True
+        Me.MonoFlat_ControlBox1.EnableMinimizeButton = True
+        Me.MonoFlat_ControlBox1.Location = New System.Drawing.Point(384, 15)
+        Me.MonoFlat_ControlBox1.Name = "MonoFlat_ControlBox1"
+        Me.MonoFlat_ControlBox1.Size = New System.Drawing.Size(100, 25)
+        Me.MonoFlat_ControlBox1.TabIndex = 14
+        Me.MonoFlat_ControlBox1.Text = "MonoFlat_ControlBox1"
+        '
+        'Textbox1
+        '
+        Me.Textbox1.BackColor = System.Drawing.Color.Transparent
+        Me.Textbox1.Font = New System.Drawing.Font("Stencil", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Textbox1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(176, Byte), Integer), CType(CType(183, Byte), Integer), CType(CType(191, Byte), Integer))
+        Me.Textbox1.Image = Nothing
+        Me.Textbox1.Location = New System.Drawing.Point(13, 104)
+        Me.Textbox1.MaxLength = 32767
+        Me.Textbox1.Multiline = False
+        Me.Textbox1.Name = "Textbox1"
+        Me.Textbox1.ReadOnly = False
+        Me.Textbox1.Size = New System.Drawing.Size(314, 41)
+        Me.Textbox1.TabIndex = 7
+        Me.Textbox1.Text = """Insert Process Name"""
+        Me.Textbox1.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left
+        Me.Textbox1.UseSystemPasswordChar = False
+        '
+        'Timer1
+        '
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(496, 456)
+        Me.Controls.Add(Me.MonoFlat_ThemeContainer1)
+        Me.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "Xpert Injector V2.1"
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.MonoFlat_ThemeContainer1.ResumeLayout(False)
+        Me.MonoFlat_ThemeContainer1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents MonoFlat_ThemeContainer1 As MonoFlat.MonoFlat_ThemeContainer
+    Friend WithEvents Textbox1 As MonoFlat.MonoFlat_TextBox
+    Friend WithEvents Button2 As MonoFlat.MonoFlat_Button
+    Friend WithEvents Button5 As MonoFlat.MonoFlat_Button
+    Friend WithEvents Button4 As MonoFlat.MonoFlat_Button
+    Friend WithEvents Button3 As MonoFlat.MonoFlat_Button
+    Friend WithEvents RadioButton1 As MonoFlat.MonoFlat_RadioButton
+    Friend WithEvents Button1 As MonoFlat.MonoFlat_Button
+    Friend WithEvents DLLs As ListBox
+    Friend WithEvents MonoFlat_ControlBox1 As MonoFlat.MonoFlat_ControlBox
+    Friend WithEvents Checkbox1 As MonoFlat.MonoFlat_CheckBox
+    Friend WithEvents Label1 As MonoFlat.MonoFlat_Label
+    Friend WithEvents RadioButton2 As MonoFlat.MonoFlat_RadioButton
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+End Class
